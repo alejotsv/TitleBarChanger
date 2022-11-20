7e6cf3b7deeabe5deff3d66affb7b5ce7f689f52
Alejandro Salas <asalas.wd@outlook.com>
2022-11-19 20:06:13 -0500

    Initial commit

---
+++ src/com/java21days/TitleBarChanger.java
@@ -0,0 +1,15 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TitleBarChanger extends JFrame implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        System.out.println("I was clicked");
+    }
+}
