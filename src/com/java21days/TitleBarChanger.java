5e1755f6c41cce7d6f47a861f1bdcb08835a1a0d
Alejandro Salas <asalas.wd@outlook.com>
2022-11-19 20:07:46 -0500

    Generate constructor

--- src/com/java21days/TitleBarChanger.java
+++ src/com/java21days/TitleBarChanger.java
@@ -1,11 +1,16 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TitleBarChanger extends JFrame implements ActionListener {
+    Dimension dim = new Dimension(300, 300);
 
+    TitleBarChanger(){
+        super("Guildenstern");
+    }
 
 
     @Override
